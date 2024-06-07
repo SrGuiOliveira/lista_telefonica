91da905ab2f8a2198736ca91838c0b5e20be248f
Guilherme Oliveira <gui.live012@gmail.com>
2024-06-07 14:03:12 -0300

    adição de contatos na lista

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 alert("Olá!")
+
+const form = document.getElementById('form')
+const nomes = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit',function (e){
+
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela()
+});
+
+function adicionaLinha() {
+    
+    const nomeContato = document.getElementById('nomeContato');
+    const numeroContato = document.getElementById('numeroContato');
+
+    if (telefones.includes(numeroContato.value) ) {
+        
+        alert(`O contato: ${nomeContato.value} com o telefone ${numeroContato.value} já está na lista!`)
+
+    } else {
+
+        nomes.push(nomeContato.value);
+        telefones.push(Number(numeroContato.value));
+    
+        let linha = '<tr>';
+        linha += `<td>${nomeContato.value}</td>`;
+        linha += `<td>${numeroContato.value}</td>`;
+        linha += `</tr>`;
+    
+        linhas += linha;
+
+    }
+
+    nomeContato.value = '';
+    numeroContato.value = '';
+
+
+};
+
+function atualizaTabela() {
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+
+
+};
