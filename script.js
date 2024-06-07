ae7ea9610aba77a93dc8bdaf0d5a0419125aa87a
Guilherme Oliveira <gui.live012@gmail.com>
2024-06-07 14:36:32 -0300

    fix: correção para não receber numero igual na lista e contagem de contatos

--- script.js
+++ script.js
@@ -9,38 +9,33 @@ let linhas = '';
 form.addEventListener('submit',function (e){
 
     e.preventDefault();
+    
     adicionaLinha();
-    atualizaTabela()
+    atualizaTabela();
+    calculaContatos();
 });
 
 function adicionaLinha() {
-    
     const nomeContato = document.getElementById('nomeContato');
     const numeroContato = document.getElementById('numeroContato');
 
-    if (telefones.includes(numeroContato.value) ) {
-        
-        alert(`O contato: ${nomeContato.value} com o telefone ${numeroContato.value} já está na lista!`)
-
+    if (telefones.includes(Number(numeroContato.value))) {
+        alert(`O telefone ${numeroContato.value} já está na lista!`);
     } else {
-
         nomes.push(nomeContato.value);
         telefones.push(Number(numeroContato.value));
-    
+
         let linha = '<tr>';
         linha += `<td>${nomeContato.value}</td>`;
         linha += `<td>${numeroContato.value}</td>`;
-        linha += `</tr>`;
-    
-        linhas += linha;
+        linha += '</tr>';
 
+        linhas += linha;
     }
 
     nomeContato.value = '';
     numeroContato.value = '';
-
-
-};
+}
 
 function atualizaTabela() {
 
@@ -50,3 +45,8 @@ function atualizaTabela() {
 
 
 };
+
+function calculaContatos() {
+
+    document.getElementById('calculaContatos').innerHTML = telefones.length;
+}
